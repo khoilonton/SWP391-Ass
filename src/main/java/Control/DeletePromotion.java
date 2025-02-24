5c7f407702c3cdbd018f86854f9ae2ad6da6d2b8
khoilonton <trangkhoi1512st@gmail.com>
2025-02-24 22:32:40 +0700

    sua loi code

--- src/main/java/Control/DeletePromotion.java
+++ src/main/java/Control/DeletePromotion.java
@@ -56,36 +56,41 @@ public class DeletePromotion extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-              String promoId = request.getParameter("id");
-        PromotionDAO promotionDAO = new PromotionDAO();
-
-        try {
-            boolean isDeleted = promotionDAO.deletePromotion(promoId);
-
-            if (isDeleted) {
-                response.sendRedirect("PromotionManager"); // Trở về trang danh sách sau khi xóa
-            } else {
-                request.setAttribute("errorMessage", "⚠️ Failed to delete promotion.");
-                request.getRequestDispatcher("DeletePromotion.jsp").forward(request, response);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            request.setAttribute("errorMessage", "❌ Error occurred while deleting promotion.");
-            request.getRequestDispatcher("DeletePromotion.jsp").forward(request, response);
-        }
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+           String promoId = request.getParameter("id");
+//      PromotionDAO promotionDAO = new PromotionDAO();
+//
+//       try {
+//        //  boolean isDeleted = promotionDAO.deletePromotion(promoId);
+//
+//          if (isDeleted) {
+//             response.sendRedirect("PromotionManager"); // Trở về trang danh sách sau khi xóa
+//          } else {
+//              request.setAttribute("errorMessage", "⚠️ Failed to delete promotion.");
+//               request.getRequestDispatcher("DeletePromotion.jsp").forward(request, response);
+//           }
+//       } catch (Exception e) {
+//           e.printStackTrace();
+//           request.setAttribute("errorMessage", "❌ Error occurred while deleting promotion.");
+//           request.getRequestDispatcher("DeletePromotion.jsp").forward(request, response);       
+//       }
     }
-    }
+   
 
     /**
      * Returns a short description of the servlet.
      *
      * @return a String containing servlet description
+     */    @Override
+ /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
      */
-    @Override
+
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
+
